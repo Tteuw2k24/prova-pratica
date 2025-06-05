2869428c6c875cf1354711982c781b366696a710
Tteuw2k24 <165336247+Tteuw2k24@users.noreply.github.com>
2025-06-04 22:27:36 -0300

    Add files via upload

---
+++ produtos.cs
@@ -0,0 +1,34 @@
+using System;
+
+namespace ProvaP2Pratica
+{
+    class produtos
+
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+        public int Quantidade { get; set; }
+
+        public produtos(string nome, double preco, int quantidade, int id)
+        {
+            Id = id;
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+    }
+    public class clientes
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public int CPF { get; set; }
+
+        public clientes(string nome, string Email, int cpf)
+        {
+            Nome = nome;
+            this.Email = Email;
+            CPF = cpf;
+        }
+    }
+}
