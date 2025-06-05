b090b230869a7749fc5eaa6193185df8538776e7
Tteuw2k24 <tteuw2004@gmail.com>
2025-06-04 23:11:34 -0300

    EXCLUI A CLASSE PRODUTO

--- produtos.cs
+++ produtos.cs
@@ -2,13 +2,7 @@ using System;
 
 namespace provaPratica
 {
-    class produtos
-
-    {
-        public int Id { get; set; }
-        public string Nome { get; set; }
-        public double Preco { get; set; }
-        public int Quantidade { get; set; }
+    
 
         public produtos(string nome, double preco, int quantidade, int id)
         {
